283ead7cc26eb86c536d150810810a25ca01da3b
jon wong <j@jnwng.com>
2024-08-06 11:22:32 -0400

    Fixing typos

--- src/components/products/global/index.js
+++ src/components/products/global/index.js
@@ -1,53 +1,56 @@
-import { documentationSection } from '@/shared/sections'
-import { Hero } from './Hero'
-import { Logo } from './Logo'
+import { documentationSection } from "@/shared/sections";
+import { Hero } from "./Hero";
+import { Logo } from "./Logo";
 
 export const global = {
-  name: 'Metaplex',
-  headline: 'Developer Hub',
-  description: 'One place for all Metaplex developer resources.',
-  path: '',
+  name: "Metaplex",
+  headline: "Developer Hub",
+  description: "One place for all Metaplex developer resources.",
+  path: "",
   isFallbackProduct: true,
   icon: <Logo />,
-  github: 'https://github.com/metaplex-foundation',
-  className: 'accent-sky',
-  heroes: [{ path: '/', component: Hero }],
+  github: "https://github.com/metaplex-foundation",
+  className: "accent-sky",
+  heroes: [{ path: "/", component: Hero }],
   sections: [
     {
-      ...documentationSection(''),
+      ...documentationSection(""),
       navigation: [
         {
-          title: 'Overview',
+          title: "Overview",
           links: [
-            { title: 'Introduction', href: '/' },
-            { title: 'Programs and Tools', href: '/programs-and-tools' },
+            { title: "Introduction", href: "/" },
+            { title: "Programs and Tools", href: "/programs-and-tools" },
           ],
         },
         {
-          title: 'Resources',
+          title: "Resources",
           links: [
-            { title: 'Offical Links', href: '/official-links' },
-            { title: 'Developer Tools', href: '/developer-tools' },
+            { title: "Official Links", href: "/official-links" },
+            { title: "Developer Tools", href: "/developer-tools" },
             {
-              title: 'Understanding Programs',
-              href: '/understanding-programs',
+              title: "Understanding Programs",
+              href: "/understanding-programs",
             },
-            {title: "Metaplex Rust SDKs", href: '/guides/rust/metaplex-rust-sdks'},
-            { title: 'RPC Providers', href: '/rpc-providers' },
-            { title: 'Storage Providers', href: '/storage-providers' },
-            { title: 'Stability Index', href: '/stability-index' },
-            { title: 'Protocol Fees', href: '/protocol-fees' },
+            {
+              title: "Metaplex Rust SDKs",
+              href: "/guides/rust/metaplex-rust-sdks",
+            },
+            { title: "RPC Providers", href: "/rpc-providers" },
+            { title: "Storage Providers", href: "/storage-providers" },
+            { title: "Stability Index", href: "/stability-index" },
+            { title: "Protocol Fees", href: "/protocol-fees" },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           links: [
-            { title: 'Community Guides', href: '/community-guides' },
-            { title: 'Security', href: '/security' },
-            { title: 'Contact Us', href: '/contact' },
+            { title: "Community Guides", href: "/community-guides" },
+            { title: "Security", href: "/security" },
+            { title: "Contact Us", href: "/contact" },
           ],
         },
       ],
     },
   ],
-}
+};
